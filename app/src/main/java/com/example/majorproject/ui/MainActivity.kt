f5031d363ef962d74503ecaac95b359c01d42af9
Kshitij Girkar <girkarkshitij@gmail.com>
2021-09-25 18:36:42 +0530

    add navigation drawer

--- app/src/main/java/com/example/majorproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/majorproject/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.majorproject.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.majorproject.R
 import com.example.majorproject.adapter.SolidItemAdapter
@@ -10,9 +12,13 @@ import com.example.majorproject.daos.SolidDao
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var actionBarDrawerToggler: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpViews()
 
         startLearningButton.setOnClickListener {
             val startLearningActivityIntent = Intent(this, StartLearningActivity::class.java)
@@ -23,5 +29,22 @@ class MainActivity : AppCompatActivity() {
             val quizMenuActivityIntent = Intent(this, QuizMenuActivity::class.java)
             startActivity(quizMenuActivityIntent)
         }
+    }
+
+    private fun setUpViews(){
+        setUpDrawerLayout()
+    }
+
+    private fun setUpDrawerLayout(){
+        setSupportActionBar(topAppBar)
+        actionBarDrawerToggler = ActionBarDrawerToggle(this, main_drawer, R.string.app_name, R.string.app_name)
+        actionBarDrawerToggler.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggler.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
